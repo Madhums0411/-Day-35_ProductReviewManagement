ec2307183c96a4f2ce775c32e46df91bb622f07d
madhums0411 <madhums41175@gmail.com>
2022-09-23 20:11:16 +0530

    Create product review list

--- ProductReviewManagement/ProductReviewManagement/Program.cs
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Product Review Management Problem");
+﻿using ProductReviewManagementLINQ;
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Welcome to Product Review Management Problem");
+
+
+Console.WriteLine("Welcome To Product Review Management System");
+List<ProductReview> list = ProductReviewManagement.AddProductsReview();
+ProductReviewManagement.DisplayeProductsReview(list);
+Console.ReadLine();
